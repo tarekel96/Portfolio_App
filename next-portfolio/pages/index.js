204f1c47e4dbc5e592be4c21c239f28b59f7693b
Tarek <elhaj102@mail.chapman.edu>
2022-06-20 19:54:33 -0700

    using mui styled utility method now

--- next-portfolio/pages/index.js
+++ next-portfolio/pages/index.js
@@ -1,38 +1,36 @@
 import { useSettingsContext } from '../context/SettingsContext';
 // @ts-ignore
 import { useTheme } from '@mui/material/styles';
-import Link from 'next/link';
-import Button from '@mui/material/Button';
+import { styled } from '@mui/system';
 import IconButton from '@mui/material/IconButton';
 import Box from '@mui/material/Box';
 // @ts-ignore
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 
+const Wrapper = styled(Box)(({ theme }) => ({
+	display: 'flex',
+	width: '100%',
+	height: '100%',
+	alignItems: 'center',
+	justifyContent: 'center',
+	backgroundColor: theme.palette.background.default,
+	color: theme.palette.text.primary,
+	borderRadius: 1,
+	padding: 3
+}));
+
 const Home = () => {
 	const theme = useTheme();
 	// @ts-ignore
 	const { darkMode, toggleDarkMode } = useSettingsContext();
-	console.log(darkMode, toggleDarkMode);
 	return (
-		<Box
-			sx={{
-				display: 'flex',
-				width: '100%',
-				height: '100%',
-				alignItems: 'center',
-				justifyContent: 'center',
-				bgcolor: 'background.default',
-				color: 'text.primary',
-				borderRadius: 1,
-				p: 3
-			}}
-		>
+		<Wrapper>
 			{theme.palette.mode} mode
 			<IconButton sx={{ ml: 1 }} onClick={toggleDarkMode} color="inherit">
 				{theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
 			</IconButton>
-		</Box>
+		</Wrapper>
 	);
 };
 export default Home;
